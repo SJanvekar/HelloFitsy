6d8c28a9614a5fd502f455efd5766e6d2414e526
Salman <sjanvekar.balance@gmail.com>
2022-05-29 22:05:42 -0400

    Updated public and private files

---
+++ config/Public/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const dbconfig = require('./dbconfig')
+
+
+const connectDB = async() => {
+    try{
+        const con = await mongoose.connect(dbconfig.database,{
+            useNewUrlParser: true,
+        })
+        console.log('MongoDB Connected:', con.connection.host)
+    }
+    catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
