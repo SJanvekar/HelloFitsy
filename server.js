1ea8e8d3d5c9fb015015c4c0d0a6d6528c44b79c
Salman <sjanvekar.balance@gmail.com>
2022-05-29 22:32:12 -0400

    Didn't save... FUCK

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use(routes)
 app.use(passport.initialize())
-require('./config/Private/passport')(passport)
+require('./config/Public/passport')(passport)
 
 const PORT  = process.env.PORT || 8888
 
