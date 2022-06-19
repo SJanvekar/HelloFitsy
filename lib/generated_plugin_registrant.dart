ab68989b8d4f324b38f52eb66f7657d169750559
Salman <sjanvekar.balance@gmail.com>
2022-06-19 19:13:56 -0400

    Profile upload implemented

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,6 +8,7 @@
 
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -16,6 +17,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FluttertoastWebPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
   SignInWithApplePlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
--- lib/screen/login/components/profilePictureUpload.dart
+++ lib/screen/login/components/profilePictureUpload.dart
@@ -1,17 +1,41 @@
-// ignore_for_file: prefer_const_constructors, file_names
-
 import 'dart:io';
-
 import 'package:balance/constants.dart';
 import 'package:balance/screen/login/components/personalInfo.dart';
 import 'package:balance/sharedWidgets/loginFooterButton.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfilePictureUpload extends StatelessWidget {
-  const ProfilePictureUpload({Key? key}) : super(key: key);
+// var image = AssetImage('assets/images/profilePictureDefault.png');
+var image;
+
+class ProfilePictureUpload extends StatefulWidget {
+  ProfilePictureUpload({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePictureUpload> createState() => _ProfilePictureUploadState();
+}
+
+class _ProfilePictureUploadState extends State<ProfilePictureUpload> {
+  File? image;
+
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) {
+        final image = AssetImage('assets/images/profilePictureDefault.png');
+        return;
+      }
+
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch (e) {
+      print('Failed to pick image $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,17 +88,45 @@ class ProfilePictureUpload extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 167.0),
-            child: Center(child: profileImage()),
+            padding: const EdgeInsets.only(top: 135.0),
+            child: Center(
+                child: Stack(
+              children: [
+                Container(
+                    height: 155,
+                    width: 155,
+                    child: Column(
+                      children: [
+                        ClipOval(
+                          child: image != null
+                              ? Image.file(
+                                  image!,
+                                  width: 155,
+                                  height: 155,
+                                  fit: BoxFit.cover,
+                                )
+                              : Image.asset(
+                                  'assets/images/profilePictureDefault.png',
+                                  height: 155,
+                                  width: 155,
+                                ),
+                        ),
+                      ],
+                    ))
+              ],
+            )),
           ),
           pageTitle(),
           pageText(),
           Padding(
             padding: const EdgeInsets.only(top: 162, bottom: 15),
-            child: LoginFooterButton(
-                buttonColor: strawberry,
-                textColor: snow,
-                buttonText: 'Upload Picture'),
+            child: GestureDetector(
+              child: LoginFooterButton(
+                  buttonColor: strawberry,
+                  textColor: snow,
+                  buttonText: 'Upload Picture'),
+              onTap: () => {pickImage(ImageSource.gallery)},
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(bottom: 45.0),
@@ -89,10 +141,10 @@ class ProfilePictureUpload extends StatelessWidget {
 
 Widget profileImage() {
   return Stack(
-    children: const [
+    children: [
       CircleAvatar(
         radius: 65,
-        backgroundImage: AssetImage('assets/images/profilePictureDefault.png'),
+        backgroundImage: image,
         backgroundColor: snow,
       )
     ],
