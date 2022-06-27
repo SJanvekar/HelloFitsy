e038ae4be14c081d4796aec1c578a4f12a2521ba
Salman <sjanvekar.balance@gmail.com>
2022-06-26 20:56:00 -0400

    Made Login screen responsive to all devices

--- lib/screen/login/login.dart
+++ lib/screen/login/login.dart
@@ -13,92 +13,86 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Container(
-        color: snow,
-        child: Column(
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.only(
-                  left: 23.5,
-                  right: 23.5,
-                  top: 250,
-                  bottom: 80,
-                ),
-                child: Hero(
+        body: Container(
+      color: snow,
+      child: Column(
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(
+                left: 23.5,
+                right: 23.5,
+                top: 311,
+              ),
+              child: Hero(
                   tag: 'typeface',
-                  child: Container(
-                      height: 260,
-                      width: 240,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                        image: AssetImage(
-                          "assets/images/Typeface.png",
-                        ),
-                      ))),
-                )),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 10,
-                  ),
-                  child: GestureDetector(
-                    child: LoginFooterButton(
-                        buttonColor: strawberry,
-                        textColor: snow,
-                        buttonText: "Start training"),
-                    onTap: () {
-                      print("Start Training Button Pressed");
-                      Navigator.of(context).push(CupertinoPageRoute(
-                          fullscreenDialog: true,
-                          builder: (context) => PersonalInfo()));
-                    },
-                  ),
+                  child: Image.asset(
+                    'assets/images/Typeface.png',
+                    height: 142,
+                    width: 195,
+                  ))),
+          Spacer(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 10,
                 ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 100),
-              child: GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                child: Center(
-                  child: Container(
+                child: GestureDetector(
+                  child: LoginFooterButton(
+                      buttonColor: strawberry,
+                      textColor: snow,
+                      buttonText: "Start training"),
+                  onTap: () {
+                    print("Start Training Button Pressed");
+                    Navigator.of(context).push(CupertinoPageRoute(
+                        fullscreenDialog: true,
+                        builder: (context) => PersonalInfo()));
+                  },
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 100),
+            child: GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              child: Center(
+                child: Container(
+                  height: 60,
+                  width: 110,
+                  child: SizedBox(
                     height: 60,
                     width: 110,
-                    child: SizedBox(
-                      height: 60,
-                      width: 110,
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                          child: Text("Log In",
-                              style: TextStyle(
-                                fontFamily: 'SFDisplay',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: strawberry,
-                              )),
-                        ),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Text("Log In",
+                            style: TextStyle(
+                              fontFamily: 'SFDisplay',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: strawberry,
+                            )),
                       ),
                     ),
                   ),
                 ),
-                onTap: () {
-                  print("Sign In Button Pressed");
-                  Navigator.of(context).push(
-                    CupertinoPageRoute(
-                      fullscreenDialog: true,
-                      builder: (context) {
-                        return SignIn();
-                      },
-                    ),
-                  );
-                },
               ),
+              onTap: () {
+                print("Sign In Button Pressed");
+                Navigator.of(context).push(
+                  CupertinoPageRoute(
+                    fullscreenDialog: true,
+                    builder: (context) {
+                      return SignIn();
+                    },
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     ));
   }
