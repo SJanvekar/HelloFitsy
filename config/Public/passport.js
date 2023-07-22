d8f02538c718c960d3c04f99e419b00eeb55d04f
William Xian <wxian@Williams-Mac-mini.local>
2023-07-22 16:52:16 -0400

    re-added config/Public files to git repo

---
+++ config/Public/passport.js
@@ -0,0 +1,25 @@
+var JwtStrat = require('passport-jwt').Strategy
+var ExtractJwt = require('passport-jwt').ExtractJwt
+
+var User = require('../../api/models/User')
+var config = require('../Private/dbconfig')
+
+module.exports = function(passport){
+    var opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+    opts.secretOrKey = config.secret;
+    
+    passport.use(new JwtStrat(opts, function(jwt_payload, done) {
+        User.findOne({id: jwt_payload.id}, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                done(null, user);
+            } else {
+                done(null, false);
+            
+            }
+        });
+    }));
+}
